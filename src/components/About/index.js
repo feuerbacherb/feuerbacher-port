d626b6c6184dbde495e734dd9de9e7eac694d0b3
Brent Feuerbacher <feuerbacherb@gmail.com>
2020-08-02 19:31:22 -0500

    trying something new with the resume download

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -31,7 +31,7 @@ export default class About extends Component {
                         </div>
                         <div className="columns download">
                         <p>
-                            <a href={resume} className="button"><i className="fa fa-download" />Download Resume</a>
+                            <a href={resume} className="button" target="_blank" rel="noopener noreferrer"><i className="fa fa-download" />Download Resume</a>
                         </p>
                         </div>
                     </div> 
