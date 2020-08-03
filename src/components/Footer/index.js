29fcce86d6c6be987b61e47db8a67911c7050ecf
Brent Feuerbacher <feuerbacherb@gmail.com>
2020-08-02 19:00:29 -0500

    put copyright in for ceevee10

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -10,6 +10,10 @@ export default class Footer extends Component {
                     <ul className="social-links"><li><a href="https://github.com/feuerbacherb"><i className="fa fa-github" /></a></li>
                         <li><a href="https://www.linkedin.com/in/brent-feuerbacher-2871604a/"><i className="fa fa-linkedin" /></a></li>
                     </ul>
+                    <ul className="copyright">
+              <li>&copy; Copyright 2017 Tim Baker</li>
+              <li>Design by <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a></li>
+           </ul>
                     </div>
                     <div id="go-top"><a className="smoothscroll" title="Back to Top" href="#home"><i className="icon-up-open" /></a></div>
                 </div>
